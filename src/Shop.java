5c4a9b6552afe13db4acd8d74491827da0432b3b
BT_4N2_28 <afasfd@gmail.com>
2023-12-19 13:09:49 -0500

    -TH-2:Modified the method getCostOfItem() in the Shop class by changing item.equals to item.equalsIgnoreCase, which makes it not case-sensitive -TH-1:Modified the processChoice() method in the TreasureHunter class by adding the option to "Hunt for treasure". If the user enters H or h, the genTreasure() method is called.

--- src/Shop.java
+++ src/Shop.java
@@ -154,23 +154,23 @@ public class Shop
      */
     public int getCostOfItem(String item)
     {
-        if (item.equals("Water"))
+        if (item.equalsIgnoreCase("Water"))
         {
             return WATER_COST;
         }
-        else if (item.equals("Rope"))
+        else if (item.equalsIgnoreCase("Rope"))
         {
             return ROPE_COST;
         }
-        else if (item.equals("Machete"))
+        else if (item.equalsIgnoreCase("Machete"))
         {
             return MACHETE_COST;
         }
-        else if (item.equals("Horse"))
+        else if (item.equalsIgnoreCase("Horse"))
         {
             return HORSE_COST;
         }
-        else if (item.equals("Boat"))
+        else if (item.equalsIgnoreCase("Boat"))
         {
             return BOAT_COST;
         }
--- src/TreasureHunter.java
+++ src/TreasureHunter.java
@@ -110,6 +110,7 @@ public class TreasureHunter
             System.out.println("(S)ell something at the shop.");
             System.out.println("(M)ove on to a different town.");
             System.out.println("(L)ook for trouble!");
+            System.out.println("(H)unt for treasure!");
             System.out.println("Give up the hunt and e(X)it.");
             System.out.println();
             System.out.print("What's your next move? ");
@@ -145,6 +146,9 @@ public class TreasureHunter
         else if (choice.equals("X") || choice.equals("x"))
         {
             System.out.println("Fare thee well, " + hunter.getHunterName() + "!");
+        }
+        else if (choice.equals("H") || choice.equals("h")) {
+            hunter.genTreasure();
         }
         else
         {
